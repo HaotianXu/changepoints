366a3278d96d347e3606ae088ef35bfa9af5b263
Haotian Xu <haotian.xu2@icloud.com>
2021-10-21 16:31:24 +0100

    update univariate_mean_online

--- R/univariate_mean_online.R
+++ R/univariate_mean_online.R
@@ -18,13 +18,13 @@
 #' @examples
 #' y_vec = rnorm(300) + c(rep(0, 150), rep(1, 150))
 #' train_vec = rnorm(200)
-#' temp1 = online.univar.one(y_vec = y_vec, train_vec = train_vec, alpha = 0.05, permu_num = 100)
+#' temp1 = online.univar(y_vec = y_vec, train_vec = train_vec, alpha = 0.05, permu_num = 100)
 #' temp1$cpt_hat
 #' temp1$b_vec
-#' temp2 = online.univar.one(y_vec = y_vec, train_vec = train_vec, gamma = 300, permu_num = 100)
+#' temp2 = online.univar(y_vec = y_vec, train_vec = train_vec, gamma = 300, permu_num = 100)
 #' temp2$cpt_hat
 #' temp2$b_vec
-online.univar.one = function(y_vec, b_vec = NULL, train_vec = NULL, alpha = NULL, gamma = NULL, permu_num = NULL, ...){
+online.univar = function(y_vec, b_vec = NULL, train_vec = NULL, alpha = NULL, gamma = NULL, permu_num = NULL, ...){
   if(!is.null(b_vec)){
     if(length(y_vec) - length(b_vec) != 1){
       stop("b_vec should be the vector of thresholds b_t with t >= 2.")
@@ -95,75 +95,75 @@ online.univar.one = function(y_vec, b_vec = NULL, train_vec = NULL, alpha = NULL
 }
 
 
-#' @title Online change point detection with controlled average run length.
-#' @description  Perform online change point detection via CUSUM (single change point, type 2).
-#' @param y_vec       A \code{numeric} vector of observations.
-#' @param gamma       A \code{integer} scalar of interval length (>= 2).
-#' @param tau_gamma   A \code{numeric} scalar of threshold.
-#' @param ...         Additional arguments.
+#' #' @title Online change point detection with controlled average run length.
+#' #' @description  Perform online change point detection via CUSUM (single change point, type 2).
+#' #' @param y_vec       A \code{numeric} vector of observations.
+#' #' @param gamma       A \code{integer} scalar of interval length (>= 2).
+#' #' @param tau_gamma   A \code{numeric} scalar of threshold.
+#' #' @param ...         Additional arguments.
+#' #' @return  An \code{integer} scalar of estimated change point location.
+#' #' @export
+#' #' @author Haotian Xu
+#' #' @examples
+#' #' TO DO
+#' online.univar.one2 = function(y_vec, gamma, tau_gamma, ...){
+#'   t = 1
+#'   FLAG = 0
+#'   while(FLAG == 0 & t <= length(y_vec)){
+#'     t = t + 1
+#'     e = max(t-gamma, 0)
+#'     cusum_vec = sapply((e+1):(t-1), function(s) sqrt((t-s)*(s-e)/(t-e)) * abs(mean(y_vec[(e+1):s]) - mean(y_vec[(s+1):t])))
+#'     FLAG = 1 - prod(cusum_vec <= tau_gamma)
+#'   }
+#'   return(t)
+#' }
+#' 
+#' 
+#' 
+#' #' @title Online change point detection via CUSUM (single change point, type 3).
+#' #' @description Perform online change point detection via CUSUM (single change point, type 3).
+#' #' @param y_vec       A \code{numeric} vector of observations.
+#' #' @param tau_vec     A \code{numeric} vector of thresholds at time t>= 1.
+#' #' @param ...         Additional arguments.
+#' #' @return  An \code{integer} scalar of estimated change point location.
+#' #' @export
+#' #' @author Haotian Xu
+#' #' @examples
+#' #' TO DO
+#' online.univar.one3 = function(y_vec, tau_vec, ...){
+#'   if(length(y_vec) != length(tau_vec)){
+#'     stop("y_vec and tau_vec should have the same length.")
+#'   }
+#'   t = 1
+#'   FLAG = 0
+#'   while(FLAG == 0 & t <= length(y_vec)){
+#'     t = t + 1
+#'     J = floor(log2(t))
+#'     j = 0
+#'     while(j < J & FLAG == 0){
+#'       j = j + 1
+#'       s_j = t - 2^(j-1)
+#'       cusum = sqrt((t-s_j)*s_j/t) * abs(mean(y_vec[1:s_j]) - mean(y_vec[(s_j+1):t]))
+#'       FLAG = (cusum > tau_vec[t])
+#'     }
+#'   }
+#'   return(t)
+#' }
+
+
+#' @title Online change point detection with potentially multiple change points.
+#' @description Perform Online change point detection with potentially multiple change points.
+#' @param y_vec     A \code{numeric} vector of observations.
+#' @param b_vec     A \code{numeric} vector of thresholds b_t at time t>= 2.
+#' @param ...       Additional arguments.
 #' @return  An \code{integer} scalar of estimated change point location.
 #' @export
 #' @author Haotian Xu
 #' @examples
 #' TO DO
-online.univar.one2 = function(y_vec, gamma, tau_gamma, ...){
-  t = 1
-  FLAG = 0
-  while(FLAG == 0 & t <= length(y_vec)){
-    t = t + 1
-    e = max(t-gamma, 0)
-    cusum_vec = sapply((e+1):(t-1), function(s) sqrt((t-s)*(s-e)/(t-e)) * abs(mean(y_vec[(e+1):s]) - mean(y_vec[(s+1):t])))
-    FLAG = 1 - prod(cusum_vec <= tau_gamma)
-  }
-  return(t)
-}
-
-
-
-#' @title Online change point detection via CUSUM (single change point, type 3).
-#' @description Perform online change point detection via CUSUM (single change point, type 3).
-#' @param y_vec       A \code{numeric} vector of observations.
-#' @param tau_vec     A \code{numeric} vector of thresholds at time t>= 1.
-#' @param ...         Additional arguments.
-#' @return  An \code{integer} scalar of estimated change point location.
-#' @export
-#' @author Haotian Xu
-#' @examples
-#' TO DO
-online.univar.one3 = function(y_vec, tau_vec, ...){
-  if(length(y_vec) != length(tau_vec)){
-    stop("y_vec and tau_vec should have the same length.")
-  }
-  t = 1
-  FLAG = 0
-  while(FLAG == 0 & t <= length(y_vec)){
-    t = t + 1
-    J = floor(log2(t))
-    j = 0
-    while(j < J & FLAG == 0){
-      j = j + 1
-      s_j = t - 2^(j-1)
-      cusum = sqrt((t-s_j)*s_j/t) * abs(mean(y_vec[1:s_j]) - mean(y_vec[(s_j+1):t]))
-      FLAG = (cusum > tau_vec[t])
-    }
-  }
-  return(t)
-}
-
-
-#' @title Online change point detection via CUSUM (multiple change points).
-#' @description Perform Online change point detection via CUSUM (multiple change points).
-#' @param y_vec       A \code{numeric} vector of observations.
-#' @param tau_vec     A \code{numeric} vector of thresholds at time t>= 2.
-#' @param ...         Additional arguments.
-#' @return  An \code{integer} scalar of estimated change point location.
-#' @export
-#' @author Haotian Xu
-#' @examples
-#' TO DO
-online.univar.multi = function(y_vec, tau_vec, ...){
-  if(length(y_vec) - length(tau_vec) != 1){
-    stop("tau_vec should be the vector of thresholds at time t >= 2.")
+online.univar.multi = function(y_vec, b_vec, ...){
+  if(length(y_vec) - length(b_vec) != 1){
+    stop("b_vec should be the vector of thresholds b_t with t >= 2.")
   }
   cpt = NULL
   e = 0
@@ -172,7 +172,7 @@ online.univar.multi = function(y_vec, tau_vec, ...){
   while(t < length(y_vec)-1){
     t = t + 1
     cusum_vec = sapply((e+1):(t-1), function(s) sqrt((t-s)*(s-e)/(t-e)) * abs(mean(y_vec[(e+1):s]) - mean(y_vec[(s+1):t])))
-    FLAG = 1 - prod(cusum_vec <= tau_vec[t])
+    FLAG = 1 - prod(cusum_vec <= b_vec[t])
     if(FLAG == 1){
       cpt = c(cpt, t)
       FLAG = 0
