10aae709f2ecd2badda87f8a4656d6d84fb5595d
Haotian Xu <haotian.xu2@icloud.com>
2021-11-10 22:45:10 +0000

    update multivariate_nonparametric

--- R/multivariate_nonparametric.R
+++ R/multivariate_nonparametric.R
@@ -6,25 +6,22 @@
 #' @param h         A \code{integer} scalar of bandwidth parameter.
 #' @return  A \code{numeric} scalar of the CUSUM statistic based on KS distance.
 #' @noRd
-CUSUM.KS.multivariate = function(Y, s, e, t, h){
+CUSUM.KS.multivariate = function(Y, W, s, e, t, h){
   p = dim(Y)[1]
-  n = dim(Y)[2]
   n_st = t - s + 1
   n_se = e - s + 1
   n_te = e - t
   aux = Y[,s:t]
-  aux = aux[which(is.na(aux)==FALSE)]
-  temp1 = kde(t(aux), gridsize = 30, eval.points = t(Y), H = h*diag(p))
+  temp1 = ks::kde(t(aux), gridsize = 30, eval.points = t(W), H = h*diag(p))
   aux = Y[,(t+1):e]
-  aux = aux[which(is.na(aux)==FALSE)]
-  temp2 = kde(t(aux), gridsize = 30, eval.points = t(Y), H = h*diag(p))
+  temp2 = ks::kde(t(aux), gridsize = 30, eval.points = t(W), H = h*diag(p))
   result = sqrt(n_st * n_te / n_se) * max(abs(temp1$estimate - temp2$estimate))
   return(result)
 }
 
 
-#' @title Wild binary segmentation for multivariate nonparametric change points detection
-#' @description TO DO
+#' @title Wild binary segmentation for multivariate nonparametric change points detection.
+#' @description Perform wild binary segmentation for multivariate nonparametric change points detection.
 #' @param Y         A \code{numeric} matrix of observations with with horizontal axis being time, and vertical axis being dimension.
 #' @param s         A \code{integer} scalar of starting index.
 #' @param e         A \code{integer} scalar of ending index.
@@ -33,19 +30,18 @@ CUSUM.KS.multivariate = function(Y, s, e, t, h){
 #' @param h         A \code{numeric} scalar of bandwidth parameter.
 #' @param level     Should be fixed as 0.
 #' @param ...       Additional arguments.
-#' @return     A \code{list} with the structure:
-#' \itemize{
-#'  \item S:           A vector of estimated changepoints (sorted in strictly increasing order).
-#'  \item Dval:        A vector of values of CUSUM statistic based on KS distance.
-#'  \item Level:       A vector representing the levels at which each change point is detected.
-#'  \item Parent:      A matrix with the starting indices on the first row and the ending indices on the second row.
-#' } 
+#' @return     A \code{list} with the following structure:
+#'  \item{S}{A vector of estimated change points (sorted in strictly increasing order)}
+#'  \item{Dval}{A vector of values of CUSUM statistic based on KS distance}
+#'  \item{Level}{A vector representing the levels at which each change point is detected}
+#'  \item{Parent}{A matrix with the starting indices on the first row and the ending indices on the second row}
 #' @export
-#' @author Haotian Xu
+#' @author Oscar Hernan Madrid Padilla & Haotian Xu
 #' @examples
 #' new_MWBS
 #' 
-MNWBS = function(Y, s, e, Alpha, Beta, h, delta = ceiling(max(p, h^(-p))), level = 0, ...){
+MNWBS = function(Y, W, s, e, Alpha, Beta, h, delta, level = 0, ...){
+  print("aaa")
   Alpha_new = pmax(Alpha, s)
   Beta_new = pmin(Beta, e)
   idx = which(Beta_new - Alpha_new > 2*delta)
@@ -68,7 +64,7 @@ MNWBS = function(Y, s, e, Alpha, Beta, h, delta = ceiling(max(p, h^(-p))), level
       e_star = Beta_new[m] - delta
       temp = rep(0, e_star - s_star + 1)
       for(t in s_star:e_star){
-        temp[t-s_star+1] = CUSUM.KS.multivariate(Y, Alpha_new[m], Beta_new[m], t, h)
+        temp[t-s_star+1] = CUSUM.KS.multivariate(Y, W, Alpha_new[m], Beta_new[m], t, h)
       }
       best_value = max(temp)
       best_t = which.max(temp) + s_star - 1
@@ -77,8 +73,8 @@ MNWBS = function(Y, s, e, Alpha, Beta, h, delta = ceiling(max(p, h^(-p))), level
     }
     m_star = which.max(a)
   }
-  temp1 = MNWBS(Y, s, b[m_star]-1, Alpha, Beta, h, level)
-  temp2 = MNWBS(Y, b[m_star], e, Alpha, Beta, h, level)
+  temp1 = MNWBS(Y, Y, s, b[m_star]-1, Alpha, Beta, h, delta, level)
+  temp2 = MNWBS(Y, Y, b[m_star], e, Alpha, Beta, h, delta, level)
   S = c(temp1$S, b[m_star], temp2$S)
   Dval = c(temp1$Dval, a[m_star], temp2$Dval)
   Level = c(temp1$Level, level, temp2$Level)
@@ -90,55 +86,130 @@ MNWBS = function(Y, s, e, Alpha, Beta, h, delta = ceiling(max(p, h^(-p))), level
 
 
 
+#' A function to compute change points based on the multivariate nonparametic method.
+#' @param Y         A \code{numeric} matrix of observations with with horizontal axis being time, and vertical axis being dimension.
+#' @param W         A copy of the matrix Y, it can be Y itself.
+#' @param Alpha     A \code{integer} vector of starting indices of random intervals.
+#' @param Beta      A \code{integer} vector of ending indices of random intervals.
+#' @param h         A \code{numeric} scalar of bandwith parameter.
+#' @return A vector of estimated change points
+#' @author Oscar Hernan Madrid Padilla
 #' @export
-MNWBS.tune = function(Y, W, Alpha, Beta, h, len_tau, delta){
-  len_time = ncol(Y)
-  temp1 = MNWBS(W, 1, len_time, Alpha, Beta, h, delta, level = 0)  
+#' @examples 
+#' n = 300
+#' v = c(floor(n/3), 2*floor(n/3)) # location of change points
+#' p = 20
+#' Y = matrix(0, p, n) # matrix for data
+#' mu0 = rep(0, p) # mean of the data
+#' mu1 = rep(0, p)
+#' mu1[1:floor(p/2)] = 1
+#' Sigma0 = diag(p) #Covariance matrices of the data
+#' Sigma1 = diag(p)*2
+#' # Generate data
+#' for(t in 1:n){
+#'   if(t < v[1] || t > v[2]){
+#'      Y[,t] = MASS::mvrnorm(n = 1, mu0, Sigma0)
+#'   }
+#'   if(t >= v[1] && t < v[2]){
+#'      Y[,t] = MASS::mvrnorm(n = 1, mu1, Sigma1)
+#'   }
+#' }## close for generate data
+#' M = 50
+#' intervals = WBS.intervals(M = M, lower = 1, upper = ncol(Y)) #Random intervals
+#' K_max = 30
+#' h = 5*(K_max*log(n)/n)^{1/p} # bandwith
+#' S = MNWBS.tune(Y, Y, intervals$Alpha, intervals$Beta, h, delta = 5)
+#' S
+MNWBS.tune = function(Y, W, Alpha, Beta, h, delta){
+  p = nrow(Y)
+  obs_num = ncol(Y)
+  temp1 = MNWBS(Y, W, 1, obs_num, Alpha, Beta, h, delta, level = 0)  
   Dval = temp1$Dval
   aux = sort(Dval, decreasing = TRUE)
-  tau_grid = rev(aux[1:min(len_tau,length(Dval))])
+  len_tau = 30
+  tau_grid = rev(aux[1:min(len_tau,length(Dval))]) - 10^{-30}
   B_list = c()
   for(j in 1:length(tau_grid)){
-    aux = threshold.BS(temp1, tau_grid[j])$change_points[,1]
+    print(j)
+    aux = threshold.BS(temp1, tau_grid[j])$cpt_hat[,1]
     if(length(aux) == 0){
-      break;
+      break
     }
     B_list[[j]] = sort(aux)
   }
   B_list = unique(B_list)
-  O_set = B_list[[1]]
-  lambda = log(sum(N))/1.5#2.5#1.5#2#2.555#
-  v_n = 100
-  for(m in 1:(length(B_list)-1)){
-    eta = min(setdiff(O_set, B_list[[m+1]]))
-    B_set_ext = c(1, B_list[[m+1]], len_time)
-    k = which.max(B_list[[m+1]] < eta)
-    eta1 = B_list[[m+1]][k]
-    eta2 = B_list[[m+1]][k+1]
-    pval = rep(0, length(v_n))
-    vec_s = matrix(0, v_n, len_time)
-    for(ind_v in 1:v_n){
-      vec = runif(len_time)
-      vec = vec/sqrt(sum(vec^2))
-      vec_s[ind_v,] = vec 
-      if(ind_v < len_time + 1){
-        vec = rep(0, len_time)
-        vec[ind_v] = 1
-      }
-      aux = CUSUM.KS(Y%*%vec, eta1, eta2, eta, rep(1,len_time))  
-      pval[ind_v] = exp(-2*aux^2)
-      st_aux = aux
-    }
-    pval_adj = p.adjust(pval, method ="fdr")
-    min_pval = min(pval_adj)
-    if(min_pval < 0.0005){
-      break;
-    }
-    if(min_pval > 0.1){
-      O_set = NULL
-      return(O_set)
-    }
-    O_set = B_list[[m+1]]
+  if(length(B_list) == 0){
+    return(NULL)
   }
-  return(O_set)
+  if(length(B_list[[1]]) == 0){
+    return(B_list[[1]])
+  }
+  indicator = 0
+  lambda = 1.8^2
+  h_grid = 1
+  v_n = 100
+  best_count = matrix(0, length(B_list)+1, v_n)
+  min_pval = 10
+  for(j in 1:length(B_list)){
+    B2 = B_list[[j]]
+    if(j < length(B_list)){
+      B1 = B_list[[j+1]]
+    }else if(j == length(B_list)){
+      B1 = NULL
+    }
+    temp = setdiff(B2,B1)
+    st = 10^{-10}
+    for(l in 1:length(temp)){
+      eta = temp[l]
+      if(length(B1) == 0){
+        eta1 = 1
+        eta2 = obs_num
+      }else if(length(B1) > 0){
+        for(k in 1:length(B1)){
+          if(B1[k] > eta){
+            break
+          }
+        }
+        if(B1[k] > eta){
+          eta2 = B1[k]
+          if(k == 1)
+            eta1 = 1
+          if(k > 1)
+            eta1 = B1[k-1]+1
+        }
+        if(B1[k] < eta){
+          eta1 = B1[k] + 1
+          eta2 = obs_num
+        }
+      }######## if length(B1) > 0
+      pval = rep(0, length(v_n))
+      vec_s = matrix(0, v_n, p)
+      for(ind_v in 1:v_n){
+        vec = runif(p)
+        vec = vec/sqrt(sum(vec^2))
+        vec_s[ind_v,] = vec 
+        if(ind_v < p+1){
+          vec = rep(0, p)
+          vec[ind_v] = 1
+        }
+        aux = CUSUM.KS(t(t(Y)%*%vec), eta1, eta2, eta, rep(1,obs_num))  
+        pval[ind_v] = exp(-2*aux^2)
+        st_aux = aux
+      }## for ind_v
+      pval_adj = p.adjust(pval, method ="fdr")
+      if(min_pval > min(pval_adj)){
+        min_pval = min(pval_adj)
+      }
+    }### for l
+    if(min_pval < 0.0005){
+      break
+    }
+  }##### j 
+  print(min_pval)
+  if(min_pval > .1){
+    est = NULL
+    return(est)
+  }
+  est = B_list[[j]]
+  return(est)
 }
